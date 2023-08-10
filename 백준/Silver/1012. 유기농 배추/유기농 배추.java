71f5bb2a8e5b36ab40dfec682ff37355cf104911
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2023-08-10 15:58:40 +0900

    [Silver II] Title: 유기농 배추, Time: 168 ms, Memory: 15892 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,102 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int cnt,M,N;
-    static int[][]a;
-    static int[] dx={1,-1,0,0};
-    static int[] dy={0,0,-1,1};
-    static boolean[][] visited;
-
+    static int a[][];
+    static Queue<int[]>q=new LinkedList<>();
+    static int[] dx={0,0,-1,1};
+    static int[] dy={-1,1,0,0};
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
-
         int T=Integer.parseInt(br.readLine());
-
         for(int t=0;t<T;t++){
 
-            StringTokenizer st=new StringTokenizer(br.readLine());
+            StringTokenizer st=new StringTokenizer(br.readLine()," ");
             M=Integer.parseInt(st.nextToken());
             N=Integer.parseInt(st.nextToken());
             int K=Integer.parseInt(st.nextToken());
 
-            cnt=0;
             a=new int[M][N];
-            visited=new boolean[M][N];
+            cnt=0;
 
             for(int i=0;i<K;i++){
                 st=new StringTokenizer(br.readLine());
                 int x=Integer.parseInt(st.nextToken());
                 int y=Integer.parseInt(st.nextToken());
+
                 a[x][y]=1;
 
             }
 
             for(int i=0;i<M;i++){
                 for(int j=0;j<N;j++){
-                    if (!visited[i][j] && a[i][j]==1){
-                        dfs(i,j);
+                    if(a[i][j]==1){
+                        bfs(i,j);
                         cnt++;
                     }
-
-
                 }
             }
-
             System.out.println(cnt);
 
-
-
-
-
-
-
-
         }
-
-
-
-
-
-
-
-
-
-
 
     }
 
-    static void dfs(int x, int y){
-        visited[x][y]=true;
+    static void bfs(int x,int y){
+        a[x][y]=0;
+        q.add(new int[]{x,y});
 
-        for(int i=0;i<4;i++){
-            int nx=x+dx[i];
-            int ny=y+dy[i];
+        while(!q.isEmpty()){
+            int[] now=q.poll();
 
-            if (nx<0 || nx>=M || ny<0 || ny>=N) continue;
-            if (visited[nx][ny]) continue;
-            if(!visited[nx][ny]&& a[nx][ny]==1) {
+            for(int i=0;i<4;i++){
+                int nx=now[0]+dx[i];
+                int ny=now[1]+dy[i];
 
-                dfs(nx,ny);
-
+                if(nx<0||nx>=M||ny<0||ny>=N) continue;
+                if(a[nx][ny]==1){
+                    a[nx][ny]=0;
+                    q.add(new int[]{nx,ny});
+                }
             }
-
-
         }
 
-
-
-
     }
-
-
-
-
 
 }
