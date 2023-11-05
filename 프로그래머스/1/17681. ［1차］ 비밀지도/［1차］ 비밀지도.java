9f8154b81a421a9a609977d22d6a96b6f3637cd2
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2023-11-05 22:47:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.00 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,32 +1,18 @@
 import java.util.*;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        Arrays.fill(answer,"");
-        String[] a1=new String[n];
-        String[] a2=new String[n];
+        String[] str=new String[n];
         
         for(int i=0;i<n;i++){
-            a1[i]=Integer.toBinaryString(arr1[i]);
-            a2[i]=Integer.toBinaryString(arr2[i]);
-            if(a1[i].length()<n) {
-                while(a1[i].length()<n) a1[i]="0"+a1[i];
-            }
-            if(a2[i].length()<n) {
-                while(a2[i].length()<n) a2[i]="0"+a2[i];
-            } 
+            str[i]=Integer.toBinaryString(arr1[i]|arr2[i]);
+            str[i]=String.format("%16s",str[i]);
+            str[i]=str[i].substring(16-n);
+            
+            str[i]=str[i].replace("1","#");
+            str[i]=str[i].replace("0"," ");
+            
         }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(a1[i].charAt(j)=='0'&&a2[i].charAt(j)=='0') {
-                    answer[i]+=" ";
-                }
-                else {answer[i]+="#";}
-            }
-        }
-        
-        
-        return answer;
+        return str;
     }
 }
