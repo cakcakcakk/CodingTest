7e7b0df91df9e94f764e44492281405147468220
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2023-11-20 14:37:46 +0900

    [Silver III] Title: 두 수의 합, Time: 412 ms, Memory: 35408 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -8,23 +8,21 @@ public class Main {
 
         int n=Integer.parseInt(br.readLine());
 
-
-        List<Integer> l=new ArrayList<>();
+        int[] l =new int[n];
+        Set<Integer> s=new HashSet<>();
 
         int cnt=0;
 
         StringTokenizer st=new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
-
-            l.add(Integer.parseInt(st.nextToken()));
+            l[i]=Integer.parseInt(st.nextToken());
+            s.add(l[i]);
         }
         int x=Integer.parseInt(br.readLine());
-
-        Collections.sort(l);
-        Set<Integer> s=new HashSet<>(l);
+        Arrays.sort(l);
 
         for(int i=0;i<n;i++){
-            if(s.contains(x-l.get(i))&&l.get(i)<x-l.get(i)) {cnt++;}
+            if(s.contains(x-l[i])&&l[i]<x-l[i]) {cnt++;}
         }
         System.out.println(cnt);
 
