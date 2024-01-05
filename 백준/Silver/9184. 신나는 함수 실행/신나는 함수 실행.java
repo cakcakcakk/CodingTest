62919ee8e751b494e5e482e0316073fc11e9b894
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-01-06 00:04:53 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 336 ms, Memory: 23148 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -1,55 +1,39 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
-public class Main {
 
-	static int[][][] dp = new int[21][21][21];
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringBuilder sb = new StringBuilder();
-		
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+public class Main{
+    static int[][][] arr=new int[21][21][21];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        StringTokenizer st;
+        int result=0;
 
-			if (a == -1 && b == -1 && c == -1) break;
-	
-			sb.append("w(" + a + ", " + b + ", " + c + ") = ").append(w(a ,b ,c)).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
-	
-	static int w(int a, int b, int c) { 
-		
-		// a, b, c가 범위를 벗어나지 않으면서 메모이제이션이 되어있는 경우
-		if(inRange(a, b, c) && dp[a][b][c] != 0) {
-			return dp[a][b][c];
-		}
-		
-		if(a <= 0 || b <= 0 || c <= 0) {
-			return 1;
-		}
-		
-		if(a > 20 || b > 20 || c > 20) {
-			return dp[20][20][20] = w(20, 20, 20);
-		}
-		
-		if(a < b && b < c) {
-			return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
-		}
-		
-		return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
-	}
-	
-	static boolean inRange(int a, int b, int c) {
-		return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20; 
-	}
+        for(int i=0;i<21;i++){
+            for(int j=0;j<21;j++){
+                for(int k=0;k<21;k++){
+                    if(i==0 || j==0 ||k==0) arr[i][j][k]=1;
+                    else if(i<j &&j<k) arr[i][j][k]=arr[i][j][k-1]+arr[i][j-1][k-1]-arr[i][j-1][k];
+                    else arr[i][j][k]=arr[i-1][j][k]+arr[i-1][j-1][k]+arr[i-1][j][k-1]-arr[i-1][j-1][k-1];
+                }
+            }
+        }
+
+        while(true){
+            st=new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+            int c=Integer.parseInt(st.nextToken());
+
+            if(a==-1&&b==-1&&c==-1) break;
+            if (a<=0 || b<=0 || c<=0) result=1;
+            else if(a>20 || b>20 ||c>20) result=arr[20][20][20];
+            else result=arr[a][b][c];
+
+            sb.append("w("+a+", "+b+", "+c+") = " ).append(result+"\n");
+        }
+        System.out.print(sb);
+    }
 }
