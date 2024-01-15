c3d467c6d7b40e4af9b8223dd286813c214bb2ca
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-01-16 02:34:55 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,70 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
+public class Main
+{   
+    static int[][] arr;
+    static int[] dx={-1,1,0,0};
+    static int[] dy={0,0,-1,1};
+    static int cnt,result,N;
+    static List<Integer> l=new ArrayList<>();
+    
+	public static void main(String[] args) throws IOException {
+	    
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    StringBuilder sb=new StringBuilder();
+	    
+	    N=Integer.parseInt(br.readLine());
+	    arr=new int[N][N];
+	    
+	    for(int i=0;i<N;i++){
+	        sb=new StringBuilder(br.readLine());
+	        for(int j=0;j<N;j++){	            
+	            arr[i][j]=sb.charAt(j)-'0';
+	        }	     
+	    }    
 
-public class Main {
-    static ArrayList<Integer> eachCount=new ArrayList<>();
-    static Queue<int[]> q=new LinkedList<>();
-    static int count,N;
-    static int a[][];
-    static int[] dx={0,0,-1,1};
-    static int[] dy={-1,1,0,0};
+	        for(int i=0;i<N;i++){
+	            for(int j=0;j<N;j++){
+	                if(arr[i][j]==1) {
+	                    arr[i][j]=0; //방문처리
+	                    cnt=1;
+	                    dfs(i,j);
+	                    result++;
+	                    l.add(cnt);
+	                }
+	            }
+	        }
+	        	        
+        System.out.println(result);
+        Collections.sort(l);
+        for(int a:l) System.out.println(a);	    
+	}
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        a=new int[N][N];
-        int cnt=0;
-
-        for(int i=0;i<N;i++){
-            String str=br.readLine();
-            for(int j=0;j<N;j++){
-                a[i][j]=str.charAt(j)-'0';
+	private static void dfs(int x, int y){
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            
+            if(range(nx,ny)&&arr[nx][ny]==1) {
+                arr[nx][ny]=0;
+                cnt++;
+                dfs(nx,ny);
             }
         }
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(a[i][j]==1){
-                    bfs(i,j);
-                    cnt++;
-                    eachCount.add(count);
-                }
-            }
-        }
-
-        Collections.sort(eachCount);
-        System.out.println(cnt);
-        for(int i:eachCount){
-            System.out.println(i);
-        }
-    }
-
-    static void bfs(int x,int y){
-        q.offer(new int[]{x,y});
-        count=1;
-        a[x][y]=0;
-
-        while(!q.isEmpty()){
-            int[] now=q.poll();
-            for(int i=0;i<4;i++){
-                int nx=now[0]+dx[i];
-                int ny=now[1]+dy[i];
-
-                if(nx<0||nx>=N|| ny<0||ny>=N) continue;
-                if(a[nx][ny]==1){
-                    a[nx][ny]=0;
-                    q.offer(new int[]{nx,ny});
-                    count++;
-                }
-            }
-        }
-
-    }
-
+	}
+	
+	private static boolean range(int nx, int ny){
+	    return nx>-1 && nx<N && ny>-1 && ny<N;
+	}
 }
