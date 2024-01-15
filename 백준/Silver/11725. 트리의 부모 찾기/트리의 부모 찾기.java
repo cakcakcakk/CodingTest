8c77b640793c51d7fddef61ec2ea79c3c45f5819
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-01-15 23:26:09 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1440 ms, Memory: 67208 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,64 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+public class Main
+{   static int[] parent;
+    static ArrayList<Integer>[] l;
+	public static void main(String[] args) throws IOException {
+	    
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st;
+	    
+	    int N=Integer.parseInt(br.readLine());
+	    parent=new int[N+1];
+	    l=new ArrayList[N+1];
 
-public class Main{
-
-    static boolean[] visited;
-    static int[] p;
-    static Queue<Integer> q=new LinkedList<>();
-    static ArrayList<Integer>[] a;
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(br.readLine());
-        visited=new boolean[N+1];
-        a=new ArrayList[N+1];
-        p=new int[N+1];
-
-        for(int i=0;i<=N;i++){
-            a[i]=new ArrayList<>();
-        }
-
-
-        for(int i=0;i<N-1;i++){
-            StringTokenizer st=new StringTokenizer(br.readLine());
-            int x=Integer.parseInt(st.nextToken());
-            int y=Integer.parseInt(st.nextToken());
-            a[x].add(y);
-            a[y].add(x);
-
-        }
-
-        bfs();
-
-        for(int i=2;i<=N;i++){
-            System.out.println(p[i]);
-        }
-
-    }
-    public static void bfs(){
-        q.offer(1);
-        visited[1]=true;
-
-        while(!q.isEmpty()){
-
-            int r=q.poll();
-            for(int i:a[r]){
-                if(!visited[i]){
-                    q.offer(i);
-                    visited[i]=true;
-                    p[i]=r;
-                }
-            }
-
-        }
-
-    }
-
+	    for(int i=0;i<=N;i++) l[i]=new ArrayList<>();
+	    
+	    for(int i=0;i<N-1;i++) {
+	        st=new StringTokenizer(br.readLine());
+	        int x=Integer.parseInt(st.nextToken());
+	        int y=Integer.parseInt(st.nextToken());
+	        
+	        l[x].add(y); l[y].add(x);
+	    }
+	    for(int i=0;i<=N;i++) Collections.sort(l[i]);
+	    
+	    bfs(1);
+	    
+	    for(int i=2;i<=N;i++) {System.out.println(parent[i]);}
+	    
+	}
+	private static void bfs(int r){
+	    Queue<Integer> q=new LinkedList<>();
+	    q.offer(r);
+	    
+	    while(!q.isEmpty()){
+	        int a=q.poll();
+	        
+	        for(int i=0;i<l[a].size();i++){
+	            int adj=l[a].get(i);
+	            if(parent[adj]==0) {parent[adj]=a; q.offer(adj);}
+	        }
+	    }
+	}
 }
