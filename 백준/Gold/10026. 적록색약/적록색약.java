12e4784425cd2d2a919aa26465a2db7a87aa6a0f
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-01-16 16:43:18 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,76 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int cnt,N;
-    static char a[][];
-    static boolean visited[][];
-    static int[] dx={0,0,-1,1};
-    static int[] dy={-1,1,0,0};
-
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        a=new char[N][N];
-        visited=new boolean[N][N];
-
-        for(int i=0;i<N;i++){
-            String str=br.readLine();
-            for(int j=0;j<N;j++){
-                a[i][j]=str.charAt(j);
-            }
-
-        }
-
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-
-                if(!visited[i][j]){
-                    dfs(i,j);
-                    cnt++;
-                }
-            }
-        }
-        System.out.print(cnt+" ");
-
-        visited=new boolean[N][N];
-        cnt=0;
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(a[i][j]=='G'){
-                    a[i][j]='R';
-                }
-            }
-        }
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(!visited[i][j]){
-                    dfs(i,j);
-                    cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
-        }
-    
-        static void dfs(int x,int y){
-            visited[x][y]=true;
-
-            for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-
-                if(nx<0 ||nx>=N || ny<0 ||ny>=N) continue;
-                if(a[x][y]==a[nx][ny] && !visited[nx][ny]){
-                    dfs(nx,ny);
-                }
-            }
-        }
-
+public class Main
+{   static int[][] rgb,rrb;
+    static int[] dx={-1,1,0,0};
+    static int[] dy={0,0,-1,1};
+    static int N;
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringBuilder sb=new StringBuilder();
+	    int cnt=0;
+	    N=Integer.parseInt(br.readLine());
+	    
+        rgb=new int[N][N];
+        rrb=new int[N][N];
+	    
+	    for(int i=0;i<N;i++){
+	        sb=new StringBuilder(br.readLine());
+	        for(int j=0;j<N;j++){
+	            switch(sb.charAt(j)) {
+	                
+	                case 'R':
+	                   rgb[i][j]=rrb[i][j]=1;
+	                   break;
+	                case 'G':
+	                    rgb[i][j]=2;  rrb[i][j]=1;
+	                    break;
+	               default:
+	                    rgb[i][j]=rrb[i][j]=3;
+	            } //switch	            
+	        } 
+	    } // insert RGB
+	    	    
+	    // rgb_dfs
+	    for(int i=0;i<N;i++){
+	        for(int j=0;j<N;j++){
+	            if(rgb[i][j]!=-1) {
+	                int v=rgb[i][j];
+	                rgb[i][j]=-1;
+	                dfs(i,j,v);
+	                cnt++;
+	            }
+	        }
+	    }
+	    sb=new StringBuilder();
+	    sb.append(cnt).append(" ");
+	    cnt=0;
+	    
+	    // change_rgb_to_rrb
+	    for(int i=0;i<N;i++) rgb[i]=Arrays.copyOf(rrb[i],N);
+	    
+	    //rrb_dfs
+	    for(int i=0;i<N;i++){
+	        for(int j=0;j<N;j++){
+	            if(rgb[i][j]!=-1) {
+	                int v=rgb[i][j];
+	                rgb[i][j]=-1;
+	                dfs(i,j,v);
+	                cnt++;
+	            }
+	        }
+	    }
+	    sb.append(cnt).append("\n");
+		System.out.print(sb);
+	}
+	private static void dfs(int x, int y, int v){
+	    
+	    for(int i=0;i<4;i++){
+	        int nx=x+dx[i];
+	        int ny=y+dy[i];
+	        
+	        if(range(nx,ny)&&rgb[nx][ny]==v) {
+	            rgb[nx][ny]=-1;
+	            dfs(nx,ny,v);	            
+	        }
+	    }
+	}
+	private static boolean range(int nx ,int ny) { return nx>-1 && nx<N && ny>-1 && ny<N;}
 }
