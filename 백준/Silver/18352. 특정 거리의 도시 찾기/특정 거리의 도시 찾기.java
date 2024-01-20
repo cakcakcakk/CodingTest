350160917c1e0219e6f65c76e7f23b827db853b7
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-01-20 11:04:50 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1128 ms, Memory: 269036 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,64 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    static Queue<Integer>q=new LinkedList<>();
-    static ArrayList<Integer>[]a;
+public class Main
+{   
+    static boolean cnt;
     static int[] dist;
-    static boolean zero;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-
-        int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
-        int K=Integer.parseInt(st.nextToken());
-        int X=Integer.parseInt(st.nextToken());
-
-        a=new ArrayList[N+1];
-        dist=new int[N+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-
-        for(int i=0;i<=N;i++){
-            a[i]=new ArrayList<>();
-        }
-
-        for(int i=0;i<M;i++){
-            st=new StringTokenizer(br.readLine());
-            int A=Integer.parseInt(st.nextToken());
-            int B=Integer.parseInt(st.nextToken());
-            a[A].add(B);
-        }
-
-        dijkstra(X);
-
-        for(int i=1;i<=N;i++){
-            if(dist[i]==K){
-                System.out.println(i);
-                zero=true;
-            }
-        }
-
-        if(!zero){
-            System.out.println(-1);
-        }
-    }
-
-    static void dijkstra(int X){
-        dist[X]=0;
-        q.add(X);
-
-        while(!q.isEmpty()){
-            int now=q.poll();
-            for(int t:a[now]){
-                if(dist[t]>dist[now]+1){
-                    dist[t]=dist[now]+1;
-                    q.add(t);
-                }
-            }
-        }
-    }
+    static ArrayList<Integer>[] arr;
+    static Queue<Integer> q=new LinkedList<>();
+    static final int INF=Integer.MAX_VALUE;
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st=new StringTokenizer(br.readLine());
+	    
+	    int N=Integer.parseInt(st.nextToken());
+	    int M=Integer.parseInt(st.nextToken());
+	    int K=Integer.parseInt(st.nextToken());
+	    int X=Integer.parseInt(st.nextToken());
+	    
+	    dist=new int[N+1];
+	    Arrays.fill(dist,INF);
+	    
+	    arr=new ArrayList[N+1];
+	    for(int i=0;i<=N;i++) arr[i]=new ArrayList<>();
+	    	    
+	    for(int i=0;i<M;i++) {
+	        st=new StringTokenizer(br.readLine());
+	        int a=Integer.parseInt(st.nextToken());
+	        int b=Integer.parseInt(st.nextToken());
+	        arr[a].add(b);
+	    }
+	    dijkstra(X);
+	    
+	    for(int i=1;i<=N;i++) {
+	        if(dist[i]==K) {
+	            System.out.println(i);
+	            cnt=true;
+	        }
+	    }
+	    
+	    if(!cnt) System.out.println(-1); 		
+	}
+	
+	private static void dijkstra(int x){
+	    dist[x]=0;
+	    q.offer(x);
+	    
+	    while(!q.isEmpty()){
+	        int now=q.poll();
+	        
+	        for(int i=0;i<arr[now].size();i++){
+	            int next=arr[now].get(i);
+	            if(dist[next]> dist[now]+1) {
+	                dist[next]=dist[now]+1;
+	                q.offer(next);
+	            }
+	        }
+	    }
+	}
 }
