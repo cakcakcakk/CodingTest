6f6b0e90196cefcc6d74e9e0fc74d5d6780e8099
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-01-29 23:43:26 +0900

    [Gold IV] Title: 최단경로, Time: 1032 ms, Memory: 111684 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,77 +1,77 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main{
-    static final int INF=Integer.MAX_VALUE;
-    static ArrayList<Node>[] a;
+public class Main
+{   
+    static int V,E,K;
     static int[] dist;
-    static PriorityQueue<Node> q=new PriorityQueue<>();
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-
-        int V=Integer.parseInt(st.nextToken());
-        int E=Integer.parseInt(st.nextToken());
-        int K=Integer.parseInt(br.readLine());
-
-        a=new ArrayList[V+1];
-        dist=new int[V+1];
-
-        for(int i=0;i<=V;i++) a[i]=new ArrayList<>();
-        Arrays.fill(dist,INF);
-
-        for(int i=0;i<E;i++){
-            st=new StringTokenizer(br.readLine());
-            int u=Integer.parseInt(st.nextToken());
-            int v=Integer.parseInt(st.nextToken());
-            int w=Integer.parseInt(st.nextToken());
-
-            a[u].add(new Node(v,w));
-        }
-
-        dijkstra(K);
-
-        for(int i=1;i<=V;i++){
-            if(dist[i]==INF) System.out.println("INF");
-            else System.out.println(dist[i]);
-        }
-
-    }
-
-    static void dijkstra(int K){
-        dist[K]=0;
-        q.add(new Node(K,0));
-
-        while(!q.isEmpty()){
-            Node now=q.poll();
-
-            for(int i=0;i<a[now.v].size();i++){
-                Node next=(Node)a[now.v].get(i);
-                if(dist[next.v]>now.w+next.w){
-                    dist[next.v]=now.w+next.w;
-                    q.add(new Node(next.v,dist[next.v]));
-                }
-            }
-
-        }
-    }
-
+    static PriorityQueue<Node> pq=new PriorityQueue<>();
+    static List<Node>[] list;
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st=new StringTokenizer(br.readLine());
+	    
+	    V=Integer.parseInt(st.nextToken());
+	    E=Integer.parseInt(st.nextToken());
+	    
+	    K=Integer.parseInt(br.readLine());
+	    
+	    dist=new int[V+1];
+	    Arrays.fill(dist,Integer.MAX_VALUE);
+	    
+	    list=new ArrayList[V+1];
+	    for(int i=0;i<=V;i++) list[i]=new ArrayList<>();
+	    
+	    for(int i=0;i<E;i++){
+	        st=new StringTokenizer(br.readLine());
+	        int u=Integer.parseInt(st.nextToken());
+	        int v=Integer.parseInt(st.nextToken());
+	        int w=Integer.parseInt(st.nextToken());
+	        list[u].add(new Node(v,w));
+	    }
+	    
+	    dijkstra(K);
+	    
+	    for(int i=1;i<=V;i++){
+	        if(dist[i]==Integer.MAX_VALUE) System.out.println("INF");
+	        else System.out.println(dist[i]);
+	    }
+		
+	}
+	
+	private static void dijkstra(int k){
+	    dist[k]=0;
+	    pq.offer(new Node(k,0));
+	   
+	    
+	    while(!pq.isEmpty()){
+	        Node now=pq.poll();
+	        
+	        if(now.w>dist[now.v]) continue;
+	        
+	        for(Node next:list[now.v]) {
+	            if(dist[next.v]>dist[now.v]+next.w) {
+	                dist[next.v]=dist[now.v]+next.w;
+	                pq.offer(new Node(next.v,dist[next.v]));
+	                
+	            }
+	        }
+	    }
+	}
 }
-class Node implements Comparable<Node>{
-    int v,w;
 
+class Node implements Comparable<Node>{
+    public int v,w;
+    
     public Node(int v,int w){
         this.v=v;
         this.w=w;
     }
+    
     @Override
-    public int compareTo(Node n){
-        return this.w-n.w;
+    public int compareTo(Node o){
+        return this.w-o.w;
     }
+    
 }
