726352f2dfe32188384f22ddab7b349491a11071
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-02-07 02:48:55 +0900

    [Gold IV] Title: 연구소, Time: 768 ms, Memory: 300352 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,93 +1,81 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main{
-
-    static int max=Integer.MIN_VALUE;
-    static int N,M;
-    static int[][]a;
-    static int[][]b;
+public class Main
+{   static int N,M,cnt,max;
+    static int zero;
+    static int[][] a;
+    static int[][] b;
     static int[] dx={-1,1,0,0};
     static int[] dy={0,0,-1,1};
 
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st=new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-        a=new int[N+1][M+1];
-
-        for(int i=1;i<=N;i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=1;j<=M;j++){
-                a[i][j]=Integer.parseInt(st.nextToken());
-            }
-        }
-
-        dfs(0);
+	public static void main(String[] args) throws IOException {
+	    
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		a=new int[N][M];
+		b=new int[N][M];
+		
+		for(int i=0;i<N;i++){
+		    st=new StringTokenizer(br.readLine());
+		    for(int j=0;j<M;j++){
+		        a[i][j]=Integer.parseInt(st.nextToken());
+		        b[i][j]=a[i][j];
+		        if(a[i][j]==0) zero++;
+		    }
+		}	        
+		dfs(0);		
         System.out.println(max);
+	}
+	
+	private static void dfs(int w){
+	    if(w==3) {
+	        bfs();
+	        return;
+	    }
+	    for(int i=0;i<N;i++){
+	        for(int j=0;j<M;j++){
+	            if(a[i][j]==0) {
+	                
+	                a[i][j]=1;
+	                dfs(w+1);
+	                a[i][j]=0;
+	            }
+	        }
+	    }
+	}
+	
+	private static void bfs(){
+	    Queue<int[]> q=new LinkedList<>();
+	    cnt=zero-3;
+	      
+	    for(int i=0;i<N;i++){
+	        for(int j=0;j<M;j++){
+	            b[i][j]=a[i][j];
 
-    }
+	            if(b[i][j]==2) {
+	                q.offer(new int[]{i,j});
+	            }	            
+	        }	       
+	    }
+	    
+	    while(!q.isEmpty()){
+	        int[] now=q.poll();
+	        for(int i=0;i<4;i++){
+	            int nx=now[0]+dx[i];
+	            int ny=now[1]+dy[i];
+	            
+	            if(nx>-1&&nx<N&&ny>-1&&ny<M&&b[nx][ny]==0) {
+	                b[nx][ny]=2;
+	                cnt--;
+	                q.offer(new int[]{nx,ny});
+	            }
+	        }
+	    }
 
-    static void dfs(int w){
-        if(w==3){
-            bfs();
-            return;
-        }
-
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=M;j++){
-                if(a[i][j]==0){
-                    a[i][j]=1;
-                    dfs(w+1);
-                    a[i][j]=0;
-                }
-            }
-        }
-    }
-
-    static void bfs(){
-        Queue<int[]>q=new LinkedList<>();
-        b=new int[N+1][M+1];
-
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=M;j++){
-                b[i][j]=a[i][j];
-                if(b[i][j]==2) {
-                    q.add(new int[]{i,j});
-                }
-            }
-        }
-
-        while(!q.isEmpty()){
-            int[] now=q.poll();
-
-            for(int i=0;i<4;i++){
-                int nx=now[0]+dx[i];
-                int ny=now[1]+dy[i];
-
-                if(nx<1 ||nx>N||ny<1||ny>M) continue;
-                if (b[nx][ny]==0){
-                    b[nx][ny]=2;
-                    q.add(new int[]{nx,ny});
-                }
-            }
-        }
-
-        int cnt=0;
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=M;j++){
-                if(b[i][j]==0){
-                    cnt++;
-                }
-            }
-        }
-        max=Math.max(cnt,max);
-    }
+	    max=Math.max(cnt,max);	    
+	}	
 }
