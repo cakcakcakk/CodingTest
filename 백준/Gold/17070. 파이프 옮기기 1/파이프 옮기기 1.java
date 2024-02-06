97d3825bf7fd578dace18783dde3090e6e87eb5f
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-02-07 01:12:10 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 224 ms, Memory: 15980 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,74 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main{
+public class Main
+{   static int N,cnt;
     static int[][] a;
-    static int cnt,N;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        N=Integer.parseInt(br.readLine());
-
-        a=new int[N+1][N+1];
-
-        for(int i=1;i<=N;i++){
-            st=new StringTokenizer(br.readLine());
-            for(int j=1;j<=N;j++){
-                a[i][j]=Integer.parseInt(st.nextToken());
-            }
-        }
-        dfs(1,2,0);
-
+	public static void main(String[] args) throws IOException {
+	    
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		N=Integer.parseInt(br.readLine());
+		a=new int[N][N];
+		
+		for(int i=0;i<N;i++){
+		    StringTokenizer st=new StringTokenizer(br.readLine());
+		    for(int j=0;j<N;j++){
+		        a[i][j]=Integer.parseInt(st.nextToken());
+		    }
+		}
+		dfs(0,1,0);
+		
         System.out.println(cnt);
+	}
+	
+	private static void dfs(int r, int c, int state){
 
-    }
-
-    static void dfs(int x,int y,int dir){
-        if(x==N&&y==N){
-            cnt++;
-            return;
-        }
-
-        if(dir==0){
-            if(y+1<=N && a[x][y+1]==0){
-                dfs(x,y+1,0);
-            }
-            if (diagonal(x,y)){
-                dfs(x+1,y+1,2);
-            }
-        }
-        else if (dir==1){
-            if(x+1<=N && a[x+1][y]==0){
-                dfs(x+1,y,1);
-            }
-            if (diagonal(x,y)){
-                dfs(x+1,y+1,2);
-            }
-        }
-        else {
-            if(y+1<=N&&a[x][y+1]==0){
-                dfs(x,y+1,0);
-            }
-
-            if(x+1<=N&&a[x+1][y]==0){
-                dfs(x+1,y,1);
-            }
-
-            if(diagonal(x,y)){
-                dfs(x+1,y+1,2);
-            }
-        }
-    }
-
-    static boolean diagonal(int x,int y){
-        if((x+1<=N)&&(y+1<=N)&&a[x][y+1]==0&&a[x+1][y]==0&&a[x+1][y+1]==0){
-            return true;
-        }
-        else return false;
-
-    }
+	    if(r==N-1&&c==N-1) {
+	        cnt++;
+	        return;
+	    }
+	    
+	    if(state==0) {
+	        if(c+1<N &&a[r][c+1]==0) {
+	            dfs(r,c+1,0);
+	        }
+	        if (r+1<N&&c+1<N&&a[r][c+1]==0&&a[r+1][c]==0&&a[r+1][c+1]==0) {
+	            dfs(r+1,c+1,2);
+	        }
+	    }
+	    else if (state==1) {
+	        if(r+1<N&&a[r+1][c]==0) {
+	            dfs(r+1,c,1);
+	        }
+	        
+	        if(r+1<N&&c+1<N&&a[r][c+1]==0&&a[r+1][c]==0&&a[r+1][c+1]==0) {
+	            dfs(r+1,c+1,2);
+	        }
+	    }
+	    else {
+	        if(c+1<N&&a[r][c+1]==0) {
+	            dfs(r,c+1,0);
+	        }
+	        if (r+1<N&&a[r+1][c]==0) {
+	            dfs(r+1,c,1);
+	        }
+	        if (r+1<N&&c+1<N&&a[r][c+1]==0&&a[r+1][c]==0&&a[r+1][c+1]==0) {
+	            dfs(r+1,c+1,2);
+	        }
+	    }
+	    
+	}
 }
