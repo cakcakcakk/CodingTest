9a0d8352110194d73a9d4f6c1cd6d42ac48b3df9
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-02-08 00:41:13 +0900

    [Gold IV] Title: 알파벳, Time: 1820 ms, Memory: 300948 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int R,C,max,cnt;
+    static char[][] a;
+    static Set<Character> s=new HashSet<>();
+    static int[] dx={-1,1,0,0};
+    static int[] dy={0,0,-1,1};
+    
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st=new StringTokenizer(br.readLine());
+	    
+	    R=Integer.parseInt(st.nextToken());
+	    C=Integer.parseInt(st.nextToken());
+	    a=new char[R][C];
+	    
+	    for(int i=0;i<R;i++){
+	        StringBuilder sb=new StringBuilder(br.readLine());
+	        for(int j=0;j<C;j++){
+	            a[i][j]=sb.charAt(j);
+	        }
+	    }
+	    
+	    dfs(0,0);	    
+		System.out.println(max);
+	}
+	private static void dfs(int r, int c){
+	    s.add(a[r][c]);
+	    cnt++;
+	    max=Math.max(max,cnt);
+	    
+	    for(int i=0;i<4;i++){
+	        int nx=r+dx[i];
+	        int ny=c+dy[i];
+	        
+    	    if(nx>-1&&nx<R&&ny>-1&&ny<C&&!s.contains(a[nx][ny])) {
+
+    	        dfs(nx,ny);
+    	        cnt--;
+    	        s.remove(a[nx][ny]);  	        
+    	    }	        
+	    }	    
+	}
+
+}
