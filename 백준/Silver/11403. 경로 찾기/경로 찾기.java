28ffe48d3dfe7698636803a9a42770ef8948affd
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-02-28 11:44:31 +0900

    [Silver I] Title: 경로 찾기, Time: 372 ms, Memory: 17112 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -3,41 +3,37 @@ import java.util.*;
 
 public class Main
 {
-    static int N;
+    static int N,cnt;
     static int[][] a;
     
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	    
 	    N=Integer.parseInt(br.readLine());
+	    
 	    a=new int[N][N];
 	    
 	    for(int i=0;i<N;i++){
 	        StringTokenizer st=new StringTokenizer(br.readLine());
 	        for(int j=0;j<N;j++){
 	            a[i][j]=Integer.parseInt(st.nextToken());
-	        }     
-	    }
-	    
-	    fw();
-	    
-	    for(int i=0;i<N;i++){
-	        for(int j=0;j<N;j++){
-	            System.out.print(a[i][j]+" ");
 	        }
-	        System.out.println();
 	    }
-	}
-	
-	private static void fw(){
+	    
 	    for(int k=0;k<N;k++){
 	        for(int i=0;i<N;i++){
 	            for(int j=0;j<N;j++){
-	                if(a[i][k]==1 && a[k][j]==1){
-	                    a[i][j]=1;
-	                }
+	                if(a[i][k]==1 && a[k][j]==1) a[i][j]=1;
 	            }
-	        }     
+	            
+	        }
 	    }
+
+	    for(int[] i:a){
+	        for(int j:i) System.out.print(j+" ");
+	        System.out.println();
+	    }
+        
 	}
+	
+
 }
