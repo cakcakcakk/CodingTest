f837d6d6c86adeab50c886916b634c11784c656c
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-08-17 23:54:43 +0900

    [Silver I] Title: 안전 영역, Time: 196 ms, Memory: 18316 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,68 +1,71 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main
 {
+    static int[][] arr;
+    static int cnt,result,n;  //0으로 초기화될 것임
+    static int min=100;
+    static int max=1;
     static int[] dx={-1,1,0,0};
     static int[] dy={0,0,-1,1};
-    static int[][] a;
     static boolean[][] visited;
-    static int N,max,cnt;
-    static Set<Integer> s=new HashSet<>();
-    
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer st;
-	    
-	    N=Integer.parseInt(br.readLine());
-	    
-	    a=new int[N][N];
-	    visited=new boolean[N][N];
-	    
-	    for(int i=0;i<N;i++){
-	        st=new StringTokenizer(br.readLine());
-	        for(int j=0;j<N;j++){
-	            a[i][j]=Integer.parseInt(st.nextToken());
-	            if(s.contains(a[i][j])) continue;
-	            s.add(a[i][j]);
-	        }
-	    }
-	    	    
-        if(s.size()==1) {System.out.println(1); return;}
-        
-        List<Integer>l=new ArrayList<>(s);
-        
-        for(int height:l){
+    static StringBuilder sb=new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n=Integer.parseInt(br.readLine());
+
+        arr=new int[n][n];
+
+        for(int i=0;i<n;i++){
+            st=new StringTokenizer(br.readLine());
+            for(int j=0;j<n;j++){
+                arr[i][j]=Integer.parseInt(st.nextToken());
+                min=Math.min(min,arr[i][j]);
+                max=Math.max(max,arr[i][j]);
+            }
+        }
+
+        if(min==max) {
+            System.out.println(1);
+            return;
+        }
+
+        for(int h=min;h<=max;h++) {
             cnt=0;
-            visited=new boolean[N][N];
-            
-            for(int i=0;i<N;i++){
-                for(int j=0;j<N;j++){
-                    if(a[i][j]>height&&!visited[i][j]) {
-                        dfs(i,j,height);
+            visited=new boolean[n][n];
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++) {
+
+                    if(!visited[i][j]&&arr[i][j]>h) {
+
+                        visited[i][j]=true;
+                        dfs(i,j,h);
                         cnt++;
+
                     }
                 }
             }
-            max=Math.max(max,cnt);
+            result=Math.max(cnt,result);
         }
-	    
-	    System.out.println(max);
-		
-	}
-	
-	private static void dfs(int r, int c, int h) {
-        visited[r][c]=true;
-        
-        for(int i=0;i<4;i++){
-            int nx=r+dx[i];
-            int ny=c+dy[i];
-            
-            if(nx<0||nx>=N||ny<0||ny>=N) continue;
-            if(a[nx][ny]>h&&!visited[nx][ny]) {
-                dfs(nx,ny,h);
+
+        System.out.println(result);
+    }
+
+    private static void dfs(int x, int y, int height) {
+
+        for(int i=0;i<4;i++) {
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+
+            if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
+            if(!visited[nx][ny] && arr[nx][ny]>height) {
+
+                visited[nx][ny]=true;
+                dfs(nx,ny,height);
             }
         }
-        
-	}
+    }
 }
