fa4825344b9073c6a98c447aed38b609fa1a5c5d
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2024-09-04 23:56:13 +0900

    [Silver I] Title: 스타트와 링크, Time: 372 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,60 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
-    static int N;
+public class Main
+{   
+    static int n,cnt;
     static boolean[] check;
-    static int[][] board;
-    static StringTokenizer st;
-
-    static int MIN_RESULT = Integer.MAX_VALUE;
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
-
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        check = new boolean[N+1];
-        board = new int[N+1][N+1];
-
-        for(int i=1; i<=N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=1; j<=N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        combination(0, 1);
-        System.out.println(MIN_RESULT);
-    }
-
-    static void combination(int dep, int start) {
-        if(dep == N/2) {
-            MIN_RESULT = Math.min(MIN_RESULT, getResult());
-            return;
-        }
-
-        for(int i=start; i<=N; i++) {
-
-            check[i] = true;
-            combination(dep+1, i+1);
-            check[i] = false;
-        }
-    }
-
-    static int getResult() {
-        int start = 0;
-        int link = 0;
-
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=N; j++) {
-                if(i == j) continue;
-
-                if(check[i] && check[j]) start += board[i][j];
-                if(!check[i] && !check[j]) link += board[i][j];
-            }
-        }
-        return Math.abs(start - link);
-    }
+    static int[][] arr;
+    static int min=Integer.MAX_VALUE;
+   public static void main(String[] args) throws IOException {
+       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+       n=Integer.parseInt(br.readLine());
+       
+       arr=new int[n][n];
+       check=new boolean[n];
+       
+       for(int i=0;i<n;i++){
+           StringTokenizer st=new StringTokenizer(br.readLine());
+           for(int j=0;j<n;j++) arr[i][j]=Integer.parseInt(st.nextToken());
+       }
+       makeTeam(0,0);
+       //n/2명을 골라야 함...
+      System.out.println(min);
+   }
+   
+   private static void makeTeam(int index, int depth){
+       if(depth==n/2) {
+           power();
+           return;
+       }
+       for(int i=index;i<n;i++){
+           check[i]=true;
+           makeTeam(i+1,depth+1);
+           check[i]=false;
+       }
+   }
+   
+   private static void power(){
+       int start=0,link=0;
+       
+       
+       for(int i=0;i<n;i++){
+           for(int j=0;j<n;j++){
+               if(i==j) continue;
+               if(check[i]&&check[j]) start+=arr[i][j];
+               if(!check[i]&&!check[j]) link+=arr[i][j];
+           }
+       }
+       
+       min=Math.min(Math.abs(start-link),min);
+   }
+   
 }
