f0b7453b163b4f6f066cc4efe00eef9fbf40dae2
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-01-14 11:47:57 +0900

    [Silver I] Title: 음식물 피하기, Time: 148 ms, Memory: 16488 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.java
@@ -2,61 +2,68 @@ import java.io.*;
 import java.util.*;
 
 public class Main
-{
-    static int N,M,K;
-    static int cnt,max;
-    static int[][] a;
-    static boolean[][] visited;
+{   
+    static int max,cnt;
     static int[] dx={-1,1,0,0};
     static int[] dy={0,0,-1,1};
-    static Queue<int[]> q=new LinkedList<>();
-    
+    static int n,m,k;
+    static int[][] list;
+    static boolean[][] visited;
+
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    
 	    StringTokenizer st=new StringTokenizer(br.readLine());
-	    N=Integer.parseInt(st.nextToken());
-	    M=Integer.parseInt(st.nextToken());
-	    K=Integer.parseInt(st.nextToken());
+	    n=Integer.parseInt(st.nextToken());
+	    m=Integer.parseInt(st.nextToken());
+	    k=Integer.parseInt(st.nextToken());
 	    
-	    a=new int[N+1][M+1];
-	    visited=new boolean[N+1][M+1];
+	    visited=new boolean[n+1][m+1];
+	    list=new int[n+1][m+1];
 	    
-	    for(int i=0;i<K;i++){
+	    for(int i=0;i<k;i++){
 	        st=new StringTokenizer(br.readLine());
-	        int r=Integer.parseInt(st.nextToken());
-	        int c=Integer.parseInt(st.nextToken());
-	        a[r][c]=1;
+	        int x=Integer.parseInt(st.nextToken());
+	        int y=Integer.parseInt(st.nextToken());
 	        
+	        list[x][y]=1; 
 	    }
 	    
-	    for(int i=1;i<=N;i++){
-	        for(int j=1;j<=M;j++){
-	            if(a[i][j]==1&&!visited[i][j]) {
-	                cnt=0;
-	                dfs(i,j);
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=m;j++) {
+	            if(list[i][j]==1 && !visited[i][j]) {
+	                cnt=1;
+	                bfs(i,j);
 	                max=Math.max(max,cnt);
-	            }
-	            
+	            } 
 	        }
-	    }
-	    
-		System.out.println(max);
+	    }        
+        System.out.println(max);	    
 	}
 	
-	private static void dfs(int x, int y){
-	    cnt++;
-	    visited[x][y]=true;
-	        
-        for(int i=0;i<4;i++) {
-            int nx=x+dx[i];
-            int ny=y+dy[i];
-            
-            if(nx<1||nx>N||ny<1||ny>M) continue;
-            if(a[nx][ny]==1&&!visited[nx][ny]) {
-                dfs(nx,ny);
-            }
-        }
-	        
+	private static void bfs(int r,int c){
+	    Queue<int[]> q=new LinkedList<>();
 	    
+	    q.add(new int[]{r,c});
+	    visited[r][c]=true;
+	    
+	    while(!q.isEmpty()) {
+	        int[] now=q.poll();
+	        int x=now[0];
+	        int y=now[1];
+	        
+	        for(int i=0;i<4;i++) {
+	            int nx=x+dx[i];
+	            int ny=y+dy[i];
+	            
+	            if(nx<1||nx>n||ny<1||ny>m) continue;
+	           
+	            if(list[nx][ny]==1&&!visited[nx][ny]) { // visited확인문 있어야힘!!
+	                list[nx][ny]=++cnt;
+	                visited[nx][ny]=true;
+	                q.add(new int[]{nx,ny});
+	            }
+	        }
+	    }
 	}
 }
