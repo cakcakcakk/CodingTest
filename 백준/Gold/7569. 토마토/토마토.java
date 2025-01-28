ca237847df48d208a6581cf539051a77be805c71
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-01-29 00:58:19 +0900

    [Gold V] Title: 토마토, Time: 580 ms, Memory: 105756 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,91 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-    static boolean zero;
-    static int M,N,H;
-    static int[][][]a;
+public class Main
+{
+    static int n,m,h,max;
     static int[] dx={-1,1,0,0,0,0};
-    static int[] dy={0,0,1,-1,0,0};
+    static int[] dy={0,0,-1,1,0,0};
     static int[] dz={0,0,0,0,-1,1};
-    static Queue<Point>q=new LinkedList<>();
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-
-         M=Integer.parseInt(st.nextToken());
-         N=Integer.parseInt(st.nextToken());
-         H=Integer.parseInt(st.nextToken());
-
-        a=new int[H+1][N+1][M+1];
-
-        for(int i=1;i<=H;i++){
-            for(int j=1;j<=N;j++){
-                st=new StringTokenizer(br.readLine());
-                for(int k=1;k<=M;k++){
-                    a[i][j][k]=Integer.parseInt(st.nextToken());
-                    if(a[i][j][k]==1) {q.offer(new Point(i,j,k));}
-                    else if(a[i][j][k]==0){zero=true;}
-
-                }
-            }
-        }
-
-        if(!zero){
-            System.out.println(0);
-            return;
-        }
-
-        bfs();
-        
-    }
-
-    static void bfs(){
-
-        while(!q.isEmpty()){
-            Point now=q.poll();
-
-            for(int i=0;i<6;i++){
-                int nx=now.x+dx[i];
-                int ny=now.y+dy[i];
-                int nz=now.z+dz[i];
-
-                if(nx>=1&&nx<=M&&ny>=1&&ny<=N&&nz>=1&&nz<=H && a[nz][ny][nx]==0){
-                    q.add(new Point(nz,ny,nx));
-                    a[nz][ny][nx]=a[now.z][now.y][now.x]+1;
-                }
-            }
-        }
-
-        int max=0;
-        for(int i=1;i<=H;i++){
-            for(int j=1;j<=N;j++){
-                for(int k=1;k<=M;k++){
-                    if(a[i][j][k]==0) {
-                        System.out.println(-1);
-                        return;
-                    }
-                    max=Math.max(max,a[i][j][k]);
-                }
-            }
-        }
-
-        System.out.println(max-1);
-        
-    }
-
-}
-
-class Point{
-    int x,y,z;
-    public Point(int z,int y,int x){
-        this.z=z;
-        this.y=y;
-        this.x=x;
-    }
+    static int[][][] arr;
+    static boolean ripe=true;
+    static Queue<int[]> q=new ArrayDeque<>();
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st=new StringTokenizer(br.readLine());
+	    
+	    m=Integer.parseInt(st.nextToken());
+	    n=Integer.parseInt(st.nextToken());
+	    h=Integer.parseInt(st.nextToken());
+	    
+	    arr=new int[n][m][h];
+	    
+	    for(int k=0;k<h;k++) {
+	        for(int i=0;i<n;i++) {
+	            st=new StringTokenizer(br.readLine());
+	            for(int j=0;j<m;j++) {
+	                arr[i][j][k]=Integer.parseInt(st.nextToken());
+	                if(arr[i][j][k]==0) {
+	                    ripe=false;
+	                }
+	                else if (arr[i][j][k]==1) {
+	                    q.add(new int[]{i,j,k});
+	                }
+	            }
+	        }
+	    }
+	    
+	    if(ripe) {
+	        System.out.println(0);
+	        return;
+	    }
+	    
+	    int day=bfs();
+	    
+	    for(int k=0;k<h;k++) {
+	        for(int i=0;i<n;i++) {
+	            for(int j=0;j<m;j++) {
+	                if(arr[i][j][k]==0) {
+	                    System.out.println(-1);
+	                    return;
+	                }
+	            }
+	        }
+	    }
+	    System.out.println(day);		
+	}
+	
+	static int bfs() {
+	    int day=-1;
+	    while(!q.isEmpty()) {
+	        int size=q.size();
+	        day++;
+	        
+	        for(int s=0;s<size;s++) {
+    	        int[] now=q.poll();
+    	        int x=now[0]; int y=now[1];  int z=now[2];  
+    	        
+    	        for(int i=0;i<6;i++) {
+    	            int nx=x+dx[i];
+    	            int ny=y+dy[i];
+    	            int nz=z+dz[i];
+    	            
+    	            if(nx<0||nx>=n||ny<0||ny>=m||nz<0||nz>=h) continue;
+    	            
+    	            if(arr[nx][ny][nz]==0) {
+    	                arr[nx][ny][nz]=1;
+    	                q.add(new int[]{nx,ny,nz});
+    	            }
+    	        }	            
+	        }
+	    }	    
+	    return day;
+	}
 }
