6984a709e8e47d3b61afa4c538ca3c86ea4a7e52
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-02-08 08:40:38 +0900

    [Silver II] Title: A → B, Time: 120 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,31 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main
-{
-    static long A,B;
-    static int cnt=1;
+{   
+    static Queue<Long> q=new LinkedList<>();
+    static long a,b;
     
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	    StringTokenizer st=new StringTokenizer(br.readLine());
-
-        A=Long.parseLong(st.nextToken());
-        B=Long.parseLong(st.nextToken());
-		
-		while(A!=B){
-		    if(B<A ||B%10!=1 && B%2!=0) {
-		        cnt=-1;
-		        break;
-		    }
-		    
-		    if(B%10==1) B/=10;		    
-		    else if (B%2==0)  B>>=1;
-		    
-		    cnt++;
-		}
-		System.out.println(cnt);
-		
+	    
+	    a=Long.parseLong(st.nextToken());
+	    b=Long.parseLong(st.nextToken());
+	    
+	    System.out.println(bfs());
 	}
 	
+    private static int bfs() {
+        q.add(a);
+        int cnt=1;
+
+	    while(!q.isEmpty()) {
+	        int size=q.size();   // 변수 정의 안하고 밑에 for문에서 q.size()하면 대참사남
+	        for(int i=0;i<size;i++) {
+	            long val=q.poll();
+
+    	        if(val==b) {
+    	            return cnt;
+    	        }	       
+    	        
+    	        if(val*2<=b) {   	           
+	                q.add(val*2);
+	            }
+	        
+    	        if(val*10+1<=b) {
+    	            q.add(val*10 +1);
+    	        }   	        
+	        }
+	        cnt++;
+	    } 
+	    return -1;
+    }
 }
