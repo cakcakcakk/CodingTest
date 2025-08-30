463a680dd69d2b0ddb8f5ae182fc53ac45d90de3
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-08-30 21:59:33 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,31 +1,32 @@
-import java.util.*;
 class Solution {
     public int solution(int storey) {
-    	String tmp = Integer.toString(storey);
-    	int[] arr = new int[tmp.length()];
-    	for(int i=0; i<tmp.length(); i++) {
-    		arr[i] = tmp.charAt(i) - '0';
-    	}
-    	
-    	int answer = 0;
-    	
-    	for(int i=arr.length-1; i>=0; i--) {
-    		if(arr[i] > 5) {
-    			answer += 10-arr[i];
-    			
-    			if(i==0) answer++;
-    			else arr[i-1]++;
-    		}
-    		else if(arr[i]==5 && i>0 && arr[i-1]>=5) {
-    			arr[i-1]++;
-    			answer += 5;
-    		}
-    		else {
-    			answer += arr[i];
-    		}
+        int answer = 0;
+        
+        // 돌을 최소로 사용해 0층으로 이동        
+        // dp 같다고 생각한다. storey -> 0 이 아닌, 거꾸로 생각하기?
+        // 근데 기억이 났다... 5 이하면 어쩌구 아니면 어쩌구....
 
-    	}
-    	
-    	return answer;
+        while(storey>0) {
+            int left=storey%10;
+            storey/=10;
+            
+            if(left==5) {  // 일의 자리가 5인 경우는 뒷자리까지 봐야 한다
+                if(storey%10>=5) { // 다음 앞자리가 5보다 크면 
+                    storey++;
+                    answer+=(10-left);
+                }
+                else {
+                    answer+=left;
+                }                
+            }
+            else if (left>5){  // 일의 자리 5보다 클 때
+                answer+=(10-left);
+                storey++;
+            }
+            else {          // 일의 자리 5보다 작을 때
+                answer+=left;
+            }            
+        }
+        return answer;
     }
 }
