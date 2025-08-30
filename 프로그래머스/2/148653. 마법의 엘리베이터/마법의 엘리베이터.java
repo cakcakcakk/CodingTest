1acc665c3c76a339619d5da4431db8b725107ad8
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-08-30 22:16:58 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -2,30 +2,19 @@ class Solution {
     public int solution(int storey) {
         int answer = 0;
         
-        // 돌을 최소로 사용해 0층으로 이동        
-        // dp 같다고 생각한다. storey -> 0 이 아닌, 거꾸로 생각하기?
-        // 근데 기억이 났다... 5 이하면 어쩌구 아니면 어쩌구....
-
-        while(storey>0) {
-            int left=storey%10;
-            storey/=10;
+        while(storey!=0) {
+            int upper=(storey%100)/10;
+            int num=storey%10;
             
-            if(left==5) {  // 일의 자리가 5인 경우는 뒷자리까지 봐야 한다
-                if(storey%10>=5) { // 다음 앞자리가 5보다 크면 
-                    storey++;
-                    answer+=(10-left);
-                }
-                else {
-                    answer+=left;
-                }                
+            if(num>5 || num==5 && upper>=5) {
+                storey+=10;
+                answer+=(10-num);
             }
-            else if (left>5){  // 일의 자리 5보다 클 때
-                answer+=(10-left);
-                storey++;
+            else {
+                answer+=num;
             }
-            else {          // 일의 자리 5보다 작을 때
-                answer+=left;
-            }            
+            
+            storey/=10;
         }
         return answer;
     }
