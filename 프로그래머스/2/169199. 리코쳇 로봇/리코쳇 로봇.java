6407cac8b6b0471f56cb77d96630137752824e0b
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-09-04 16:48:27 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.82 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -23,26 +23,29 @@ class Solution {
         return answer;
     }
     
-    int bfs(int x, int y, String[] board) {
+    int bfs(int sx, int sy, String[] board) {
         
         Queue<int[]> q=new ArrayDeque<>();
         boolean[][] visited=new boolean[n][m];
-        visited[x][y]=true;
+        visited[sx][sy]=true;
         
-        q.add(new int[]{x,y,0});
+        q.add(new int[]{sx,sy,0});
         
         while(!q.isEmpty()) {
             int[] now = q.poll();
             
-            if(board[now[0]].charAt(now[1])=='G') {
-                return now[2];
+            int x=now[0];
+            int y=now[1];
+            int count=now[2];              
+            
+            if(board[x].charAt(y)=='G') {
+                return count;
             }
             
             for(int i=0;i<4;i++) {
-                int nx=now[0];
-                int ny=now[1];
-                int count=now[2];  
-                                
+                int nx=x;
+                int ny=y;
+                            
                 while(nx>=0 && nx<n && ny>=0 && ny<m && board[nx].charAt(ny)!='D') {
                     nx+=dx[i];
                     ny+=dy[i];
