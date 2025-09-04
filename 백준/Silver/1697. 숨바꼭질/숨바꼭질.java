bef99ebfa86fd5e41d4772ec06d2972633a0d402
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-09-04 20:20:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 18800 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,65 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
+import java.io.*;
 public class Main
 {
-	static int N;
-	static int K;
-	
-	static int visited[] = new int[100001];
-	
-	// X-1, X+1
-	// 2*X
-	public static void main(String[] args) throws IOException
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String input = br.readLine();
-		String[] inputs = input.split(" ");
-		
-		N = Integer.valueOf(inputs[0]);
-		K = Integer.valueOf(inputs[1]);
-		
-		int result = bfs(N);
-		System.out.println(result);
-	}
+	public static void main(String[] args) throws IOException {
 
-	private static int  bfs(int node)
-	{
-		Queue<Integer> queue = new LinkedList<Integer>();
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st=new StringTokenizer(br.readLine());
+	    
+	    int n=Integer.parseInt(st.nextToken());
+	    int k=Integer.parseInt(st.nextToken());
+	    
+		System.out.println(bfs(n,k));
 		
-		queue.add(node);
-		int index = node;
-		int n = 0;
-		visited[index] = 1;
-		while (queue.isEmpty() == false)
-		{
-			n = queue.remove();
-			
-			if (n == K)
-			{
-				return visited[n]-1;
-			}
-			
-			if (n-1>=0 && visited[n-1] == 0)
-			{
-				visited[n-1] = visited[n]+1;
-				queue.add(n-1);
-			}
-			if (n+1 <= 100000 && visited[n+1] == 0)
-			{
-				visited[n+1] = visited[n]+1;
-				queue.add(n+1);
-			}
-			if (2*n <= 100000 && visited[2*n] == 0)
-			{
-				visited[2*n] = visited[n] + 1;
-				queue.add(2*n);
-			}
-		}
-		return -1;
+	}
+	
+	private static int bfs(int n, int k) {
+	    Queue<Integer> q=new LinkedList<>();
+	    int[] visited=new int[100001];
+	    
+	    q.add(n);
+	    visited[n]=1;
+	    
+	    while(!q.isEmpty()) {
+	        int now=q.poll();
+	        
+	        if(now==k) {
+	            return visited[now]-1;
+	        }
+	        
+	        for(int i=0;i<3;i++) {
+	            int next;
+	            
+	            if(i==0 && now!=0) {
+	                next=now*2;
+	            }
+	            else if (i==1) {
+	                next=now+1;
+	            }
+	            else {
+	                next=now-1; 
+	            }
+	            
+	            if(next>=0 && next<=100000 && visited[next]==0) {
+	                visited[next]=visited[now]+1;
+	                q.add(next);
+
+	            }
+	        }
+
+	    }
+	    
+	    return 1;
 	}
 }
