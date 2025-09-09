94958c1661d566f2aa04269a47fa97ccb9de6a5e
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-09-10 01:28:51 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -3,37 +3,47 @@ import java.util.*;
 
 public class Main
 {
-    static int N,cnt;
-    static int[][] a;
-    
+    static int n;
+    static int[][] arr;
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	    N=Integer.parseInt(br.readLine());
+	    StringTokenizer st;
+	    StringBuilder sb=new StringBuilder();
 	    
-	    a=new int[N][N];
+	    n=Integer.parseInt(br.readLine());
 	    
-	    for(int i=0;i<N;i++){
-	        StringTokenizer st=new StringTokenizer(br.readLine());
-	        for(int j=0;j<N;j++){
-	            a[i][j]=Integer.parseInt(st.nextToken());
-	        }
+	    arr=new int[n][n];
+	    
+	    for(int i=0;i<n;i++) {
+	        st=new StringTokenizer(br.readLine());
+	        for(int j=0;j<n;j++) {
+	            arr[i][j]=Integer.parseInt(st.nextToken());
+	        } 
 	    }
 	    
-	    for(int k=0;k<N;k++){
-	        for(int i=0;i<N;i++){
-	            for(int j=0;j<N;j++){
-	                if(a[i][k]==1 && a[k][j]==1) a[i][j]=1;
+	    for(int i=0;i<n;i++) {
+	        for(int j=0;j<n;j++) {
+	            if(arr[i][j]==1) {
+	                dfs(i,j);
 	            }
-	            
 	        }
 	    }
 
-	    for(int[] i:a){
-	        for(int j:i) System.out.print(j+" ");
-	        System.out.println();
+	    for(int i=0;i<n;i++) {
+	        for(int j=0;j<n;j++) {
+	            sb.append(arr[i][j]).append(" ");
+	        }
+	        sb.append("\n");
 	    }
-        
+	    System.out.print(sb);
+		
 	}
-	
-
+	private static void dfs(int start, int end) {
+	    for(int j=0;j<n;j++) {
+	        if(arr[end][j]==1 && arr[start][j]==0) {
+	            arr[start][j]=1;
+	            dfs(start,j);
+	        }
+	    }
+	}
 }
