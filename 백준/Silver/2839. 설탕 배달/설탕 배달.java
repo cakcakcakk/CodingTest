19a18b57f7dcb2084a1d2a2066b04f5191912cd0
cakcakcakk <87758438+cakcakcakk@users.noreply.github.com>
2025-10-31 02:50:02 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	    
+	    int n=Integer.parseInt(br.readLine());
+	    int cnt=0;
+	    
+	    while(n>0) {
+            if(n%5==0) {
+                cnt+=(n/5);
+                break;
+            }
+            n-=3;
+            cnt++;
+	    }
+		System.out.println(n>=0?cnt:-1);
+	}
+}
